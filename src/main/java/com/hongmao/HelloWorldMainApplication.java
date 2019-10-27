b7985c6ccba5fc2266ef39160039085262d7126c
zhdbest <zhd94811@163.com>
2019-10-27 22:06:42 +0800

    简易springboot应用

---
+++ src/main/java/com/hongmao/HelloWorldMainApplication.java
@@ -0,0 +1,12 @@
+package com.hongmao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+}
