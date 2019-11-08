6b6c35f956d00db2f94377dccb2c93c805e84f6f
haodong.zhao <haodong.zhao@china.zhaogang.com>
2019-11-08 13:07:59 +0800

    getPort

--- src/main/java/com/hongmao/controller/HelloController.java
+++ src/main/java/com/hongmao/controller/HelloController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author haodong.zhao
+ * @version 1.0. Added Time:2019/11/7 10:38
+ */
 @Controller
 public class HelloController {
 
@@ -11,5 +17,11 @@ public class HelloController {
     @ResponseBody
     public String hello() {
         return "Hello World!";
+    }
+
+    @RequestMapping("getPort")
+    @ResponseBody
+    public String getPort(HttpServletRequest request) {
+        return "port: " + request.getLocalPort();
     }
 }
